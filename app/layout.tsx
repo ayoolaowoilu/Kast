14e0f4f449eac6e5053e9ba7b5d41e26fee418f0
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-06-27 20:23:35 +0100

    Chore : Added the default og

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,32 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/logo.png', sizes: '16x16', type: 'image/png' },
-
     ],
     apple: [
       { url: '/logo.png', sizes: '256x256', type: 'image/png' },
     ],
+  },
+  // Add this block to link the OG image you generated
+  openGraph: {
+    title: "Kast",
+    description: "Online watch party platform.",
+    url: `https://kast-ochre.vercel.app/`, // Use your actual domain here
+    siteName: 'Kast',
+    images: [
+      {
+        url: '/og-img', // This must match the filename (without .tsx)
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Kast',
+    description: 'Online watch party platform.',
+    images: ['/og-img'], // MUST match the filename above
   },
 };
 
